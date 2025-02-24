323858e1d4f2a89679ea24d669e7ca9abe0b7afd
Drowbe <140648353+Drowbe@users.noreply.github.com>
2025-02-23 22:46:44 -0800

    removed console logs and fixed favoriting

--- scripts/panel-features.js
+++ scripts/panel-features.js
@@ -4,12 +4,6 @@ import { PanelManager } from './panel-manager.js';
 
 export class FeaturesPanel {
     constructor(actor) {
-        console.log('SQUIRE | INIT DEBUG | FeaturesPanel constructor:', {
-            actor: !!actor,
-            panelManagerExists: !!PanelManager.instance,
-            timestamp: new Date().toISOString()
-        });
-        
         this.actor = actor;
         this.features = this._getFeatures();
         // Don't set panelManager in constructor
@@ -71,21 +65,10 @@ export class FeaturesPanel {
     _getActionType(feature) {
         // In D&D5E 4.0+, we use the new activities system (plural)
         const activities = feature.system.activities;
-        
-        // Debug log the full feature data
-        console.log("SQUIRE | Feature Action Type Debug:", {
-            featureName: feature.name,
-            rawSystem: feature.system,
-            activities: activities,
-            activation: feature.system.activation,
-            cost: feature.system.activation?.cost,
-            type: feature.system.activation?.type
-        });
 
         // First check the activation directly (for backwards compatibility)
         if (feature.system.activation?.type) {
             const type = feature.system.activation.type;
-            console.log(`SQUIRE | ${feature.name} - Found activation type:`, type);
             switch(type) {
                 case 'action': return 'action';
                 case 'bonus': return 'bonus';
@@ -101,19 +84,10 @@ export class FeaturesPanel {
         // Get the first activity (usually there's only one)
         const activity = Object.values(activities)[0];
         
-        // Debug log the activity data
-        console.log("SQUIRE | Activity Data:", {
-            featureName: feature.name,
-            firstActivity: activity,
-            activationType: activity?.activation?.type,
-            allActivities: activities
-        });
-        
         if (!activity?.activation?.type) return null;
         
         // Check the activation type
         const activationType = activity.activation.type;
-        console.log(`SQUIRE | ${feature.name} - Found activity activation type:`, activationType);
         
         switch(activationType) {
             case 'action': return 'action';
@@ -125,13 +99,6 @@ export class FeaturesPanel {
     }
 
     async render(html) {
-        console.log('SQUIRE | INIT DEBUG | FeaturesPanel render start:', {
-            hasHtml: !!html,
-            hasElement: !!this.element,
-            panelManagerExists: !!PanelManager.instance,
-            timestamp: new Date().toISOString()
-        });
-
         if (html) {
             this.element = html;
         }
@@ -139,10 +106,6 @@ export class FeaturesPanel {
 
         // Get panel manager reference at render time
         this.panelManager = PanelManager.instance;
-        console.log('SQUIRE | INIT DEBUG | Got panel manager:', {
-            panelManagerExists: !!this.panelManager,
-            timestamp: new Date().toISOString()
-        });
 
         // Refresh features data
         this.features = this._getFeatures();
@@ -222,6 +185,8 @@ export class FeaturesPanel {
         panel.on('click', '.tray-buttons .fa-heart', async (event) => {
             const featureId = $(event.currentTarget).closest('.feature-item').data('feature-id');
             await FavoritesPanel.manageFavorite(this.actor, featureId);
+            // Re-render to update the UI state
+            await this.render(this.element);
         });
 
         // Feature use click (image overlay)
