ffbefecdecd23e4801b0ecf48af38cbf19942a1a
Drowbe <140648353+Drowbe@users.noreply.github.com>
2025-02-24 21:12:24 -0800

    ROLLBACK: fixed pin issue. SHOULD be good to go.

--- scripts/panel-favorites.js
+++ scripts/panel-favorites.js
@@ -1,4 +1,4 @@
-import { MODULE, TEMPLATES } from './const.js';
+import { MODULE, TEMPLATES, SQUIRE } from './const.js';
 import { PanelManager } from './panel-manager.js';
 
 export class FavoritesPanel {
